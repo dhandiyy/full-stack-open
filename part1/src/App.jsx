91de13b66f563c58fb9781ab4f806cf4c4a97c00
DhandiYudhit <dhandi.yudhit@gmail.com>
2024-08-07 18:59:29 +0700

    finished exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const total = bad+neutral+good;
 
     const chooseGood = () => {
         const newGood = good + 1;
@@ -34,6 +35,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {good-bad/2}</p>
+            <p>positive {(good/total)*100}%</p>
         </div>
     )
 }
