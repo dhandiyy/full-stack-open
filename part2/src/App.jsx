562799d5a55854744c059f706264fdb518c2f13d
DhandiYudhit <dhandi.yudhit@gmail.com>
2024-08-08 11:34:18 +0700

    initial commit for part 2

---
+++ part2/src/App.jsx
@@ -0,0 +1,17 @@
+const App = ({notes}) => {
+    const result = notes.map(note => note.id)
+    console.log(result)
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note =>
+                    <li key={note.id}>{note.content}</li>
+                )}
+            </ul>
+        </div>
+    )
+
+}
+export default App;
