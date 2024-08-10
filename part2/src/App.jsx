b37be3fd9aa3e7c2f19eba87d1d7adf0c2b7af60
DhandiYudhit <dhandi.yudhit@gmail.com>
2024-08-10 22:39:00 +0700

    finished exercises 2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,19 +1,23 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PersonForm from "./components/PersonForm.jsx";
 import Filter from "./components/Filter.jsx";
 import Persons from "./components/Persons.jsx";
+import axios from "axios";
 
 const App = () => {
 
-    const [person, setPerson] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
-
+    const [person, setPerson] = useState([])
     const [newPerson, setNewPerson] = useState({name:"", number:""})
     const [searchValue, setSearchValue] = useState("");
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons")
+            .then(response =>{
+                console.log("promise fulfilled")
+                setPerson(response.data)
+            })
+    }, []);
 
     const addPerson = (event) => {
         event.preventDefault();
