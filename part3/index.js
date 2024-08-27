2cacc31e9a38a880daedfe27cc9d43af1bc3e540
DhandiYudhit <dhandi.yudhit@gmail.com>
2024-08-27 21:36:59 +0700

    add new content backend: Express, nodemon and Http method

---
+++ part3/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json()) //json-parser
+
+let notes = [
+	{
+		id: "1",
+		content: "HTML is easy",
+		important: true
+	},
+	{
+		id: "2",
+		content: "Browser can execute only JavaScript",
+		important: false
+	},
+	{
+		id: "3",
+		content: "GET and POST are the most important methods of HTTP protocol",
+		important: true
+	}
+]
+
+const port = 3001
+
+app.get('/', (request, response) => {
+	response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+	response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+	const id = request.params.id
+	const note = notes.find(n => n.id === id)
+	if(note){
+		response.json(note)
+	}else {
+		response.status(404).end(`Not Found`) //'Not Found' akan ditampilkan
+	}
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+	const id = request.params.id
+	notes = notes.filter(n => n.id !== id)
+
+	response.status(204).end()
+})
+
+const generatedId = () => {
+	const maxId = notes.length > 0
+		? Math.max(...notes.map(n=> Number(n.id)))
+		: 0
+	return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+
+	const body = request.body
+
+	if (!body.content) {
+		return response.status(400).json({
+			error: 'Content missing'
+		})
+	}
+
+	const note = {
+		content: body.content,
+		important: Boolean(body.important) || false,
+		id: generatedId()
+	}
+
+	notes = notes.concat(note)
+
+	response.json(note)
+})
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+	console.log(`http://localhost:${port}`)
+})
