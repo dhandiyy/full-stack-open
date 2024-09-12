0f491c5a993b65ed4fbbf5843ff6dfce3651fb77
DhandiYudhit <dhandi.yudhit@gmail.com>
2024-09-12 22:05:10 +0700

    add new content: login and jsonwebtoken

--- part3/app.js
+++ part3/app.js
@@ -6,6 +6,7 @@ const app = express();
 require('express-async-errors')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose');
@@ -29,6 +30,7 @@ app.use(cors());
 app.use(express.json()); // JSON body parser
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter);
 
